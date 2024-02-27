da00af25cc6cc101ed067cc6722b81e4a78da9a6
Des <des.grainger@proton.me>
2024-02-27 23:04:37 +0000

    Created functions and switched similarity to scikit. Need to tidy up.

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+import numpy
 import openai
 # from trafilatura import fetch_url, extract, feeds, json_metadata, fetch_response
 import trafilatura
@@ -5,13 +6,16 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import credentials
-import spacy
+# import spacy
+import sklearn
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:109.0) Gecko/20100101 Firefox/116.0'}
 
 openai.api_key = credentials.openai_api_key
-nlp = spacy.load('en_core_web_lg')
+# nlp = spacy.load('en_core_web_lg')
 
 
 class ReadRss:
@@ -53,40 +57,102 @@ def to_json(articles, path):
     with open(path, 'w') as file:
         json.dump(articles, file)
 
+# guardian_texts = []
+# for text in guardian.articles_dicts:
+#     guardian_texts.append(text['text'])
 
-# commented out below while messing with guardian
+# def get_articles(articles_dicts, number):
+#     articles = []
+#     for article in articles_dicts[:number]:
+#         article_data = {}
+#         article_data['title'] = article['title']
+#         article_data['link'] = article['link']
+#         downloaded = trafilatura.fetch_response(article_data['link'].strip())
+#         result = trafilatura.extract(downloaded, include_comments=False,
+#                      include_tables=False, include_links=False)
+#         article_data['text'] = result
+#         articles.append(article_data)
+#     return articles
 
-guardian_descs = []
-for desc in guardian.articles_dicts:
-    guardian_descs.append(desc['description'])
+def get_articles(articles_dicts):
+    articles = []
+    for article in articles_dicts:
+        article_data = {}
+        article_data['title'] = article['title']
+        article_data['link'] = article['link']
+        downloaded = trafilatura.fetch_response(article_data['link'].strip())
+        result = trafilatura.extract(downloaded, include_comments=False,
+                     include_tables=False, include_links=False)
+        article_data['text'] = result
+        articles.append(article_data)
+    return articles
 
-articles = []
-
-for article in bbc.articles_dicts[:7]:
-    article_data = {}
-    article_data['title'] = article['title']
-    article_data['link'] = article['link']
-    downloaded = trafilatura.fetch_response(article_data['link'].strip())
-    result = trafilatura.extract(downloaded, include_comments=False,
-                     include_tables=False, include_links=False,)
-    article_data['text'] = result
+def summarise_article(article_text):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are a helpful summarisation tool that provides seven sentence summaries of text."},
-            {"role": "user", "content": "Please provide a seven sentence summary of the following: " + article_data['text']}
+            {"role": "user", "content": "Please provide a seven sentence summary of the following: " + article_text}
         ]
     )
-    article_data['summary'] = response['choices'][0]['message']['content']
-    processed_bbc = nlp(article_data['summary'])
-    similarities = []
-    for desc in guardian_descs:
-        processed_guardian = nlp(desc)
-        similarities.append(processed_bbc.similarity(processed_guardian))
-    max_index = similarities.index(max(similarities))
-    article_data['guardian_link'] = guardian.articles_dicts[max_index]['link']
+    return response['choices'][0]['message']['content']
 
-    articles.append(article_data)
+# def get_similar_link(article, dicts):
+#     similarities = []
+#     processed_article = nlp(article)
+#     for text in dicts:
+#         processed_comp = nlp(text['text'])
+#         similarities.append(processed_article.similarity(processed_comp))
+#     max_index = similarities.index(max(similarities))
+#     return dicts[max_index]['link']
+
+def get_similar_link(article, dicts):
+    # similarities = []
+    vectorizer = TfidfVectorizer()
+    # for text in dicts:
+    #     vectors = vectorizer.fit_transform([article, text['text']])
+    #     print(vectors.shape)
+        # similarities.append(cosine_similarity(vectors))
+    articles = [comp_article['text'] for comp_article in dicts]
+    articles.insert(0, article)
+    vectors = vectorizer.fit_transform(articles)
+    similarity = cosine_similarity(vectors[0:1], vectors)
+    similarity = numpy.delete(similarity[0],0)
+    max_index = similarity.argmax()
+    return dicts[max_index]['link']
+
+bbc_articles = get_articles(bbc.articles_dicts[:7])
+guardian_articles = get_articles(guardian.articles_dicts)
+
+for article in bbc_articles:
+    article['summary'] = summarise_article(article['text'])
+    article['guardian_link'] = get_similar_link(article['text'], guardian_articles)
+
+# for article in bbc.articles_dicts[:7]:
+#     article_data = {}
+#     article_data['title'] = article['title']
+#     article_data['link'] = article['link']
+#     downloaded = trafilatura.fetch_response(article_data['link'].strip())
+#     result = trafilatura.extract(downloaded, include_comments=False,
+#                      include_tables=False, include_links=False,)
+#     article_data['text'] = result
+#     response = openai.ChatCompletion.create(
+#         model="gpt-3.5-turbo",
+#         messages=[
+#             {"role": "system", "content": "You are a helpful summarisation tool that provides seven sentence summaries of text."},
+#             {"role": "user", "content": "Please provide a seven sentence summary of the following: " + article_data['text']}
+#         ]
+#     )
+#     article_data['summary'] = response['choices'][0]['message']['content']
+#     processed_bbc = nlp(article_data['text'])
+#     similarities = []
+#     for text in guardian_texts:
+#         processed_guardian = nlp(text)
+#         similarities.append(processed_bbc.similarity(processed_guardian))
+#     max_index = similarities.index(max(similarities))
+#     article_data['guardian_link'] = guardian.articles_dicts[max_index]['link']
+
+#     articles.append(article_data)
 
 
 # below no longer needed
@@ -103,7 +169,7 @@ for article in bbc.articles_dicts[:7]:
 #     article_data['text'] = result
 #     guardian_articles.append(article_data)
 
-to_json(articles, 'data/bbc.json')
+to_json(bbc_articles, 'data/bbc.json')
 # # to_json(guardian_articles, 'data/guardian.json') 
 # # print(guardian.articles_dicts[:7])
 
