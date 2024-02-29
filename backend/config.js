112af2667c8eb049d3edf8dc6ddbe7367c92dea0
Des <des.grainger@proton.me>
2024-02-29 21:51:59 +0000

    set up mongo db and created schema

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://desgrainger:xmo7jqu2kE6QkgSF@news-summary-db.biudxha.mongodb.net/articles-collection?retryWrites=true&w=majority&appName=news-summary-db'
---
+++ backend/models/articleModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const articleSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        link: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        },
+        summary: {
+            type: String,
+            required: true
+        },
+        guardian_link: {
+            type: String,
+            required: true
+        }
+    }, {
+    timestamps: true,
+});
+
+const Article = mongoose.model('Article', articleSchema);
