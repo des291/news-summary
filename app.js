175b00173f32abe8ef5c35ea81e7288e74352fbe
Des <des.grainger@proton.me>
2024-03-14 21:06:29 +0000

    updated schedule

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const indexRouter = require('./routes/index');
 // Schedule scraper.py to run at 06:00 and 17:00
 const rule = new schedule.RecurrenceRule();
 rule.hour = [6, 17];
+rule.minute = 0;
+rule.second = 0;
 const scraper = schedule.scheduleJob(rule, () => {
   spawn('scraper/venv/bin/python', ['scraper/scraper.py']);
 });
